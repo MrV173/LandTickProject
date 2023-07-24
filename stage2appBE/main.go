bcee636aa3e24b1de2355622ff430ad139104c4b
MrV173 <ibnuhakim81@gmail.com>
2023-07-25 06:51:19 +0700

    vercel

--- stage2appBE/main.go
+++ stage2appBE/main.go
@@ -14,10 +14,7 @@ import (
 )
 
 func main() {
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		panic("failed to load env file")
-	}
+	godotenv.Load()
 	e := echo.New()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
